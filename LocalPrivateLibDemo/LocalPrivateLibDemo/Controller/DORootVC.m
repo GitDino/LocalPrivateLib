53e3a2fcf3ed83cd706fcd3ce9308681de78e1bb
Dino <kill_death@163.com>
2018-04-16 01:59:32 +0800

    Demo初步搭建

---
+++ LocalPrivateLibDemo/LocalPrivateLibDemo/Controller/DORootVC.m
@@ -0,0 +1,25 @@
+//
+//  DORootVC.m
+//  LocalPrivateLibDemo
+//
+//  Created by 魏欣宇 on 2018/4/16.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"本地私有库";
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
